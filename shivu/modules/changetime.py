c4e6fd3556691249c75c24b367514ec5b11b3ecd
DEVIL <132729234+Devil39@users.noreply.github.com>
2024-06-25 18:46:16 +0530

    Update changetime.py

--- shivu/modules/changetime.py
+++ shivu/modules/changetime.py
@@ -53,40 +53,7 @@ async def change_time(client: Client, message: Message):
     chat_id = message.chat.id
     member = await shivuu.get_chat_member(chat_id,user_id)
         
-
-    if member.status  in GOD :
-        await message.reply_text('You are a GOD.')
-        return
-
-    try:
-        args = message.command
-        if len(args) != 2:
-            await message.reply_text('Please use: /changetime NUMBER')
-            return
-
-        new_frequency = int(args[1])
-        if new_frequency < 1:
-            await message.reply_text('The message frequency must be greater than or equal to 100.')
-            return
-
-    
-        chat_frequency = await user_totals_collection.find_one_and_update(
-            {'chat_id': str(chat_id)},
-            {'$set': {'message_frequency': new_frequency}},
-            upsert=True,
-            return_document=ReturnDocument.AFTER
-
-        )
-
-
-        await message.reply_text(f'Successfully changed {new_frequency}')
-    except Exception as e:
-        await message.reply_text(f'Failed to change {str(e)}')
-
-
-
-async def upload(update: Update, context: CallbackContext) -> None:
-    if str(update.effective_user.id)  in sudo_users:
+            if str(update.effective_user.id)  in sudo_users:
         await update.message.reply_text('Ask My Owner...')
         return
 
@@ -113,4 +80,3 @@ async def upload(update: Update, context: CallbackContext) -> None:
 await message.reply_text(f'Successfully changed {new_frequency}')
     except Exception as e:
         await message.reply_text(f'Failed to change {str(e)}')
-
