685b212da79db21faccb20701463777e17fe6816
DEVIL <132729234+Devil39@users.noreply.github.com>
2024-06-25 22:58:24 +0530

    Update changetime.py

--- shivu/modules/changetime.py
+++ shivu/modules/changetime.py
@@ -40,7 +40,7 @@ async def change_time(client: Client, message: Message):
             return_document=ReturnDocument.AFTER
         )
 
-       if user_id in SUDO:
+       if user_id in sudo_users:
            return 
 
         chat_frequency = await user_totals_collection.find_one_and_update(
