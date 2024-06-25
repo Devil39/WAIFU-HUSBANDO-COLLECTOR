9e6ce2fc44001601251f2e894b491e778574d089
DEVIL <132729234+Devil39@users.noreply.github.com>
2024-06-25 17:30:03 +0530

    Update changetime.py

--- shivu/modules/changetime.py
+++ shivu/modules/changetime.py
@@ -39,17 +39,49 @@ async def change_time(client: Client, message: Message):
 
         )
 
-            if member.status in sudo_users:
-            return 
+
+        await message.reply_text(f'Successfully changed {new_frequency}')
+    except Exception as e:
+        await message.reply_text(f'Failed to change {str(e)}')
+
+#sudo
+
+
+async def change_time(client: Client, message: Message):
+    
+    user_id = message.from_user.id
+    chat_id = message.chat.id
+    member = await shivuu.get_chat_member(chat_id,user_id)
         
+
+    if member.status not in ADMINS :
+        await message.reply_text('You are not an Admin.')
+        return
+
+    try:
+        args = message.command
+        if len(args) != 2:
+            await message.reply_text('Please use: /changetime NUMBER')
+            return
+
+        new_frequency = int(args[1])
+        if new_frequency < 1:
+            await message.reply_text('The message frequency must be greater than or equal to 100.')
+            return
+
+    
         chat_frequency = await user_totals_collection.find_one_and_update(
             {'chat_id': str(chat_id)},
             {'$set': {'message_frequency': new_frequency}},
             upsert=True,
             return_document=ReturnDocument.AFTER
-            
+
         )
+
 
         await message.reply_text(f'Successfully changed {new_frequency}')
     except Exception as e:
         await message.reply_text(f'Failed to change {str(e)}')
+
+
+
