11529a7039db273b1bd13bca046002c5079fb7a5
DEVIL <132729234+Devil39@users.noreply.github.com>
2024-06-26 00:24:54 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -6,14 +6,14 @@ class Config(object):
     sudo_users = "5005777092", "6014072313"
     GROUP_ID = -1001945999343
     TOKEN = "6997813229:AAEnU-K_dSCLC7z9ed0tB-P53PX8RfY6Eos"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
+    mongo_url = "mongodb+srv://Devil:8dTwwn8LFba8dyj@cluster0.7frtspj.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://graph.org/file/dd023dd7a7d59e53815a7.jpg", "https://graph.org/file/983c0d9ef32c50b43c91e.jpg"]
     SUPPORT_CHAT = "SD_Botssupport"
-    UPDATE_CHAT = "Collect_em_support"
+    UPDATE_CHAT = "SD_Botssupport"
     BOT_USERNAME = "Kidnap_your_waifu_Bot"
     CHARA_CHANNEL_ID = "-1001855181135"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    api_id = 20120089
+    api_hash = "dfb3ac85e07f353be8d09110e189a6e0"
 
     
 class Production(Config):
