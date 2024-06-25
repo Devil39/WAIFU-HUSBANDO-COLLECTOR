db88d29b1996400fd4ff71a0ba29db312844591c
DEVIL <132729234+Devil39@users.noreply.github.com>
2024-06-26 00:03:11 +0530

    Update changetime.py

--- shivu/modules/changetime.py
+++ shivu/modules/changetime.py
@@ -15,8 +15,7 @@ async def change_time(client: Client, message: Message):
     chat_id = message.chat.id
     member = await shivuu.get_chat_member(chat_id,user_id)
     
-    if user_id not in SUDO:
-        return 
+    
     if member.status not in ADMINS :
         await message.reply_text('You are not an Admin.')
         return
@@ -42,6 +41,17 @@ async def change_time(client: Client, message: Message):
 
         if user_id in sudo_users:
             return 
+
+        try:
+        args = message.command
+        if len(args) != 2:
+            await message.reply_text('Please use: /changetime NUMBER')
+            return
+            
+
+        new_frequency = int(args[1])
+        if new_frequency < 1:
+            return
 
         chat_frequency = await user_totals_collection.find_one_and_update(
             {'chat_id': str(chat_id)},
